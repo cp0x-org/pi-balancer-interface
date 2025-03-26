b7d6c7d0e7793fb5240481d25ed8270143983816
torys877 <torylandua@gmail.com>
2025-03-26 23:40:04 +0200

    add dockerfile, change logo

--- apps/frontend-v3/lib/components/navs/NavLogo.tsx
+++ apps/frontend-v3/lib/components/navs/NavLogo.tsx
@@ -6,17 +6,23 @@ import { BalancerLogoType } from '../imgs/BalancerLogoType'
 import { Box, Link } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import NextLink from 'next/link'
+import { Cp0xLogo } from '@/lib/components/imgs/Cp0xLogo'
 
 export function NavLogo() {
   return (
     <Box as={motion.div} variants={fadeIn}>
       <Link as={NextLink} href="/" prefetch variant="nav">
         <Box>
+          <Box display={{ base: 'none', md: 'block' }}>
+            <Cp0xLogo fill="none" height="26" viewBox="0 0 63 26" width="63" />
+          </Box>
+        </Box>
+        <Box>
           <Box display={{ base: 'block', md: 'none' }}>
             <BalancerLogo width="26px" />
           </Box>
           <Box display={{ base: 'none', md: 'block' }}>
-            <BalancerLogoType width="106px" />
+            <BalancerLogoType width="50px" />
           </Box>
         </Box>
       </Link>
